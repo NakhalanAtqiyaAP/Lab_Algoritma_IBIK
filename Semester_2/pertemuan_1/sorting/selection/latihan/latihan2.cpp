9d0cba43498d2281197392d910d85259942e6253
NakhalanAtqiya <nakhalanatqiya@gmail.com>
2026-04-02 17:28:41 +0700

    selection sorting

--- Semester_2/pertemuan_1/sorting/selection/latihan/latihan2.cpp
+++ Semester_2/pertemuan_1/sorting/selection/latihan/latihan2.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 void selectionSort(int arr[], int n)
 {
-for (int i = 0; i < n-1; i++)
+
+for (int i = 0; i < n-1; i++)           //
 {
    int min = i;
 
-   for (int j = i+1; j < n; i++)
+   for (int j = i+1; j < n; j++)
    {
     if (arr[j] < arr[min])
     {
