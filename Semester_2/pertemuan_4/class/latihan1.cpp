d2805dda3a3a372e462484561bc22c418838e7ce
NakhalanAtqiya <nakhalanatqiya@gmail.com>
2026-04-25 19:22:21 +0700

    pertemuan_4

--- Semester_2/pertemuan_4/class/latihan1.cpp
+++ Semester_2/pertemuan_4/class/latihan1.cpp
@@ -2,26 +2,44 @@
 using namespace std;
 
 class Hewan {
-    public :
+    // public :
+    // int berat;
+    // string jenisKelamin;
+
+    private :
     int berat;
     string jenisKelamin;
 
+    public :
     void berlari (){
         cout<<"Sedang berlari"<<endl;
     }
+
+    //* Setter untuk mengisi nilai private
+    void setBerat(int berat){
+       this->berat = berat;
+    }
+
+    //* Getter untuk mengembalikan nilai private
+    int getBerat(){
+        return this->berat;
+    }
+    
+
+    
 };
 
 int main()
 {
     //* cara deklarasi object - cara ke-1
     Hewan binatang1;                    // ! disini object langsung mengambil dari class Hewan
-    binatang1.berat = 30;
-    cout<<"Berat hewan 1 adalah "<<binatang1.berat<<endl;
+    binatang1.setBerat(30);
+    cout<<"Berat hewan 1 adalah "<<binatang1.getBerat()<<endl;
 
     // * cara deklarasi object - cara ke-2 (pakai pointer)
     Hewan *binatang2 = new Hewan();     // ! disini dia akan membuat sebuah object baru dengan class Hewan akan ber orentiasi ke Bintanag2. memakai panah karena pointer
-    binatang2->berat = 20;
-    cout<<"Berat hewan 2 adalah "<<binatang2->berat<<endl;
+    binatang2->setBerat(50);
+    cout<<"Berat hewan 2 adalah "<<binatang2->getBerat()<<endl;
 
     // * Fungsi berlari
     binatang1.berlari();
