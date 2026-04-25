32617c17ed8e702a2ef1f8c512a4c06df470e797
NakhalanAtqiya <nakhalanatqiya@gmail.com>
2026-04-25 19:42:03 +0700

    pertemuan_5

--- Semester_2/pertemuan_5/constracttor/latihan.cpp
+++ Semester_2/pertemuan_5/constracttor/latihan.cpp
@@ -2,11 +2,22 @@
 using namespace std;
 
 class Mahasiswa{
+    private:
+    
+
     public:
-        Mahasiswa() {
-            cout<<"Mahasiswa terdaftar"<<endl;
+        string nama;
+        int npm;
+        float gpa;
+        // ! Constractor
+        // * Fungsinya untuk deklarasi awal variabel
+        Mahasiswa(string nama, int npm, float gpa) {
+            this->nama = nama;
+            this->npm = npm;
+            this->gpa = gpa;
         }
-        ~Mahasiswa(){
+        // ! Destructor
+        ~Mahasiswa(){               
             cout<<"Mahasiswa kena DO";
         }
 };
@@ -14,12 +25,17 @@ class Mahasiswa{
 int main()
 {
     // * deklarasi biasa
-    Mahasiswa mahasiswa1;                   // ! Disini ketika dideklarasikan setelah program selesai maka object tersebut akan terhapus.
+    // Mahasiswa mahasiswa1;                   // ! Disini ketika dideklarasikan setelah program selesai maka object tersebut akan terhapus.
 
-    cout<<endl;
+    // cout<<endl;
 
     // * deklarasi menggunakan pointer
-    Mahasiswa *mahasiswa2 = new Mahasiswa;   
-    delete mahasiswa2;                      // ! object akan terhapus, ketika programnya selesai. Atau jika memakai scope {} jika scope nya habis.         
+    // Mahasiswa *mahasiswa2 = new Mahasiswa();   
+    // delete mahasiswa2;                      // ! object akan terhapus, ketika programnya selesai. Atau jika memakai scope {} jika scope nya habis. 
+    
+    Mahasiswa *mahasiswa1 = new Mahasiswa("Nakhalan", 2523123, 21.2);
+    cout<<"Nama"<<mahasiswa1->nama<<endl;
+    cout<<"Nama"<<mahasiswa1->npm<<endl;
+    cout<<"Nama"<<mahasiswa1->gpa<<endl;
     return 0;
 }
